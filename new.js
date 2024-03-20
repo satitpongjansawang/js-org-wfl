385ce054e8e62505386d7eaccebb0ceeb440db02
SATITPONG JANSAWANG <37213119+satitpongjansawang@users.noreply.github.com>
2024-03-20 21:34:07 +0700

    Delete new.js

--- new.js
+++
@@ -1,9 +0,0 @@
-function findRow(pid){
-    var source_of_row = sheet.getRange('A:A').getValues();
-    var rowIndex = source_of_row.findIndex(row => row[0] === pid);
-    if (rowIndex !== -1) {
-        return rowIndex+1;
-    }else{
-        return "";
-    }
-}
